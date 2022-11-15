88798e4455389ec9deba71e826cacb13b3edb8ee
Sean <millington.sean12@gmail.com>
2022-11-15 16:11:07 +0100

    user can now pull pokemon on a specific gen

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,6 +1,12 @@
+
+
 async function index (req, res) {
     try {
-        const pokemonJSON = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        let pokemonJSON;
+        const gens = req.params.gen.split("-")
+        if (gens.length == 1) {
+            pokemonJSON = await fetch(`https://pokeapi.co/api/v2/pokemon?${gen(gens[0])}`)
+        }
         const pokemon = await pokemonJSON.json()
         const names = pokemon.results.map(poke => poke.name)
         res.status(200).json(names)
@@ -21,6 +27,48 @@ async function show (req, res) {
     } catch (err) {
         res.status(500).json({err})
     }
+}
+
+function gen (num) {
+    let offset
+    let limit
+    switch(num) {
+        case '1':
+            offset = '0';
+            limit = '151';
+            break;
+        case '2':
+            offset = '151';
+            limit = '100';
+            break;
+        case '3':
+            offset = '251';
+            limit = '135';
+            break;
+        case '4':
+            offset = '386';
+            limit = '108';
+            break;
+        case '5':
+            offset = '494';
+            limit = '155';
+            break;
+        case '6':
+            offset = '649';
+            limit = '72';
+            break;
+        case '7':
+            offset = '721';
+            limit = '88';
+            break;
+        case '8':
+            offset = '809';
+            limit = '96';
+            break;
+        default:
+            break;
+    }
+    return `limit=${limit}&offset=${offset}`
 }
 
 module.exports = {index, show}
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pokemonController = require('../controllers/pokemon')
 
-router.get('/:range', pokemonController.index)
+router.get('/gen/:gen', pokemonController.index)
 router.get('/:name', pokemonController.show)
 
 module.exports = router
